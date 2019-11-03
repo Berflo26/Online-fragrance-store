266ecb16a8006990fc0cca77a82013ef05fb559a
Berflo26 <b.valleflores@gmail.com>
2019-11-03 11:45:45 -0600

    Diseñando página home

--- fragrance/src/component/home.js
+++ fragrance/src/component/home.js
@@ -11,7 +11,9 @@ import {
     TouchableOpacity,
     Button,
     StatusBar,
-    TouchableWithoutFeedback
+    TouchableWithoutFeedback,
+    SafeAreaView,
+    ScrollView
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -22,34 +24,45 @@ const Home = (props) => {
     
     return (  
 
-        <View style = {styles.container}>
-             <StatusBar backgroundColor="#FBCECB"/>
-             <TouchableWithoutFeedback>
-                <Icon
-                    name = "bars"
-                    color = "white"
-                    size = {27}
-                 />
-             </TouchableWithoutFeedback>
-             <Image style = {styles.logo} source = {require('../images/logo3.png')}/>
-                <Icon
-                name = "search"
-                color = "white"
-                size = {27}
-             />
-
-        </View>
+        <SafeAreaView>
         
-            
+        <ScrollView>
+        <View style = {styles.container}>
+        <StatusBar backgroundColor="#FBCECB"/>
+        <TouchableWithoutFeedback>
+           <Icon
+               name = "bars"
+               color = "white"
+               size = {27}
+            />
+         </TouchableWithoutFeedback>
+         <Image style = {styles.logo} source = {require('../images/logo3.png')}/>
+           <Icon
+           name = "search"
+           color = "white"
+           size = {27}
+         />
+         </View>   
 
-       
+         <View>
+            <Image style = {styles.containergal} source = {{uri:'http://www.fraiche.com.mx/tres-d/images/AndreaToscanoICE_nuevo.jpg'}}/>
+            <Image style = {styles.containergal} source = {{uri:'http://www.fraiche.com.mx/tres-d/images/20190328_banner_2_ilumina.jpg'}}/>
+            <Image style = {styles.containergal} source = {{uri:'http://www.fraiche.com.mx/tres-d/images/banner4You.jpg'}}/>
+            <Image style = {styles.containergal} source = {{uri:'http://www.fraiche.com.mx/tres-d/images/20190328_banner_5_torso_woman.jpg'}}/>
+            <Image style = {styles.containergal} source = {{uri:'http://www.fraiche.com.mx/tres-d/images/JonathanMas01.jpg'}}/>
+            <Image style = {styles.containergal} source = {{uri:'http://www.fraiche.com.mx/tres-d/images/BannerTS4FRAI.png'}}/>
+            <Image style = {styles.containergal} source = {{uri:'http://www.fraiche.com.mx/tres-d/images/BannerMMFRAI.png'}}/>            
+         </View> 
+        </ScrollView>
+
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container:{
         flexDirection: "row",
-        height:98,
+        height:105,
         alignItems: "center",
         justifyContent: "space-between",
         backgroundColor: '#FBCECB',
@@ -57,9 +70,18 @@ const styles = StyleSheet.create({
         
     },
     logo:{
-        width:120,
+        width:110,
         height:80,
     },
+    containergal:{
+        flexDirection:"row",
+        height: 130,
+        marginTop:2,
+        alignItems:"center",
+        justifyContent:"space-between",
+        alignItems:"center"
+         
+    }
     
 })
 
