4c10888e045bcc5d362a6ef168cd393ad8a3e725
Berflo26 <b.valleflores@gmail.com>
2019-11-02 22:29:34 -0600

    diseñando página home

--- fragrance/src/component/home.js
+++ fragrance/src/component/home.js
@@ -11,24 +11,56 @@ import {
     TouchableOpacity,
     Button,
     StatusBar,
-    
-        
+    TouchableWithoutFeedback
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Home = (props) => {
 
     const {cambiarPantalla} = props;
+    
+    return (  
 
-    return (    
+        <View style = {styles.container}>
+             <StatusBar backgroundColor="#FBCECB"/>
+             <TouchableWithoutFeedback>
+                <Icon
+                    name = "bars"
+                    color = "white"
+                    size = {27}
+                 />
+             </TouchableWithoutFeedback>
+             <Image style = {styles.logo} source = {require('../images/logo3.png')}/>
+                <Icon
+                name = "search"
+                color = "white"
+                size = {27}
+             />
 
-        <View>
-
-            <Text>
-                holas
-            </Text>
         </View>
+        
+            
+
+       
     );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection: "row",
+        height:98,
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundColor: '#FBCECB',
+        marginHorizontal: 3
+        
+    },
+    logo:{
+        width:120,
+        height:80,
+    },
+    
+})
 
 export default Home;
---
+++ fragrance/src/component/menu.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import {
+    View,
+    Text,
+    StyleSheet,
+    ImageBackground,
+    Image,
+    TextInput,
+    Dimensions,
+    TouchableOpacity,
+    Button,
+    StatusBar,
+    TouchableWithoutFeedback,
+    ScrollView
+} from 'react-native';
