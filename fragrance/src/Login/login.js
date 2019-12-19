99f23dfbc3c4ce2a8aef948dd5dc69c0b3667a4b
Berflo26 <b.valleflores@gmail.com>
2019-12-19 15:36:25 -0600

    soft

--- fragrance/src/Login/login.js
+++ fragrance/src/Login/login.js
@@ -1,170 +1,141 @@
 import React, {Component} from 'react';
 
 import {
-    View,
-    Text,
-    StyleSheet,
-    ImageBackground,
-    Image,
-    TextInput,
-    Dimensions,
-    TouchableOpacity,
-    Button,
-    StatusBar,    
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+  Button,
+  StatusBar,
 } from 'react-native';
 
 import bgImage from '../images/imglogin6.png';
 import logo from '../images/imglogin4.png';
- 
-const {width: WIDTH} = Dimensions.get('window')
 
-const Login = (props) => {
+const {width: WIDTH} = Dimensions.get('window');
 
-    const {cambiarPantalla} = props;
-    
- 
-    return(
-        <ImageBackground  source = {bgImage} style={styles.backgroundContainer}>
-           
-            <StatusBar backgroundColor="#FBCECB"
-            />
-           
-            
-                <View style = {styles.logoContainer}> 
-                    <Image source = {logo} style= {styles.logo}/>
-                    <Text style = {styles.logoText}>
-                        FRAGRANCE 
-                    </Text>
-                    <Text style = {styles.logoText2}>
-                         ¡Perfumando Nicaragua!
-                    </Text>
-                </View>
+const Login = props => {
+  const {cambiarPantalla} = props;
 
-                <View style={styles.inputContainer}>
-                    
-                    <TextInput 
-                    defaultValue={''}
-                        style= {styles.input}
-                        placeholder={'Username'}
-                        placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
-                        underlineColorAndroid= 'transparent'
-                    />
-                </View>
+  return (
+    <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+      <StatusBar backgroundColor="#FBCECB" />
 
-                <View style={styles.inputContainer}>
-            
-                <TextInput
-                    style= {styles.input}
-                    placeholder={'Password'}
-                    secureTextEntry={true}
-                    placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
-                    underlineColorAndroid= 'transparent'
-                />             
-            </View>          
+      <View style={styles.logoContainer}>
+        <Image source={logo} style={styles.logo} />
+        <Text style={styles.logoText}>FRAGRANCE</Text>
+        <Text style={styles.logoText2}>¡Perfumando Nicaragua!</Text>
+      </View>
 
-            
-                <TouchableOpacity style={styles.btnLogin} 
-                
-                onPress={
-                    () => {
-                       props.navigation.navigate('home')
-                    }
-                }
-                >
-                  <Text style={styles.text}>
-                   Login 
-                  </Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          defaultValue={''}
+          style={styles.input}
+          placeholder={'Username'}
+          placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+          underlineColorAndroid="transparent"
+        />
+      </View>
+      
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          placeholderTextColor={'rgba(255, 255, 255, 0.7)'}
+          underlineColorAndroid="transparent"
+        />
+      </View>
 
-                </TouchableOpacity>
-                
-                <Text>
-                </Text>
+      <TouchableOpacity
+        style={styles.btnLogin}
+        onPress={() => {
+          props.navigation.navigate('home');
+        }}>
+        <Text style={styles.text}>Login</Text>
+      </TouchableOpacity>
 
-                <Text style ={styles.text2}
-                onPress={
-                    () => {
-                       props.navigation.navigate('Registrar');
-                    }
-                }
-                >
-                    ¿Aún no tienes cuenta? Registrate Ya
-                </Text>
+      <Text></Text>
 
-            </ImageBackground>
-    );
-    
+      <Text
+        style={styles.text2}
+        onPress={() => {
+          props.navigation.navigate('Registrar');
+        }}>
+        
+      </Text>
+    </ImageBackground>
+  );
 };
 
-const styles = StyleSheet.create ({
-    backgroundContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    logoContainer: {
-        alignItems: 'center',
-        marginBottom: 50
-        
-    },
-    logo: {
-        width: 140,
-        height: 140,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-       
-    },
-    logoText: {
-        color: 'black',
-        fontSize: 25,
-        fontWeight: 'bold',
-        marginTop:10,
-        opacity: 0.5,
-        
-    },
-    inputContainer:{
-        marginTop: 10
-    },
-    input:{
-        width: WIDTH - 55,
-        height: 48,
-        borderRadius: 25,
-        fontSize: 25,
-        paddingLeft: 100,
-        backgroundColor: 'rgba(0, 0, 0, 0.35)',
-        marginHorizontal: 25,
-        color: 'white' ,
-        
-   },
-   btnLogin:{
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  logo: {
+    width: 140,
+    height: 130,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoText: {
+    color: 'black',
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginTop: 10,
+    opacity: 0.5,
+  },
+  inputContainer: {
+    marginTop: 10,
+  },
+  input: {
+    width: WIDTH - 55,
+    height: 48,
+    borderRadius: 25,
+    fontSize: 25,
+    paddingLeft: 100,
+    backgroundColor: 'rgba(0, 0, 0, 0.35)',
+    marginHorizontal: 25,
+    color: 'white',
+  },
+  btnLogin: {
     width: WIDTH - 55,
     height: 45,
     borderRadius: 25,
     backgroundColor: '#FBCECB',
     justifyContent: 'center',
-    marginTop:20,
-    opacity: 0.8
-    
-   },
-   text:{
+    marginTop: 20,
+    opacity: 0.8,
+  },
+  text: {
     color: 'white',
     fontSize: 25,
-    textAlign: 'center'
-   },
-   text2:{
+    textAlign: 'center',
+  },
+  text2: {
     color: '#575756',
     fontSize: 15,
     textAlign: 'center',
     fontWeight: 'bold',
-   },
-   logoText2:{
+  },
+  logoText2: {
     color: 'black',
     fontSize: 15,
     fontWeight: 'bold',
-    marginTop:5,
+    marginTop: 5,
     opacity: 0.5,
-   }
-}
-)
+  },
+});
 
 export default Login;
